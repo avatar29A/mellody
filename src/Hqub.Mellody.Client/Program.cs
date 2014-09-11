10318f9cc165b24839eaaf3d7928e1dab3a87431
Glebov Boris <avatar29A@gmail.com>
2014-09-11 21:01:55 +0600

    Провел рефакторинг проекта АПИ Вконтакта

--- src/Hqub.Mellody.Client/Program.cs
+++ src/Hqub.Mellody.Client/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
 
 namespace Hqub.Mellody.Client
 {
@@ -10,14 +11,45 @@ namespace Hqub.Mellody.Client
     {
         public static void Main(string[] args)
         {
-            Auth();
+            GetMyAudioRecords();
+
             Console.ReadKey();
         }
 
-        public static void Auth()
+        public static void GetMyAudioRecords()
         {
-            Console.WriteLine("Start auth");
+            var token = GetToken();
 
+            var api = Mellowave.Vkontakte.API.Factories.ApiFactory.Instance(token);
+            var audio = api.GetAudioProduct();
+
+            var response = audio.Get("6666100");
+
+            var counter = 0;
+            foreach (var track in response.Tracks)
+            {
+                Console.WriteLine("{0}. {1} - {2}", ++counter, track.Artist, track.Title);
+            }
+        }
+
+        public static void SearchScorpions()
+        {
+            var token = GetToken();
+
+            var api = Mellowave.Vkontakte.API.Factories.ApiFactory.Instance(token);
+            var audio = api.GetAudioProduct();
+
+            var response = audio.Search("Scorpions");
+
+            var counter = 0;
+            foreach (var track in response.Tracks)
+            {
+                Console.WriteLine("{0}. {1} - {2}", ++counter, track.Artist, track.Title);
+            }
+        }
+
+        public static string GetToken()
+        {
             var authConfigure = Core.Helpers.ConfigureHelper.GetAuthConfigure();
 
             var r = Core.Utilities.PythonInvoker.Execute(authConfigure.PythonPath, new List<string>
@@ -29,7 +61,7 @@ namespace Hqub.Mellody.Client
                 authConfigure.Scope
             });
 
-            Console.WriteLine(r);
+            return r;
         }
     }
 }
