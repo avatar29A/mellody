b3b40b681edc1f036fcbb6b5a1c18ae67c42e190
Glebov Boris <avatar29A@gmail.com>
2014-09-11 14:45:08 +0600

    add submodule

---
+++ src/Hqub.Mellody.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hqub.Mellody.Client
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Piu");
+            Console.ReadKey();
+        }
+    }
+}
