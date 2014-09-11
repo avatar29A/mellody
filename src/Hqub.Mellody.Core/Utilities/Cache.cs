fe9fb155d41c666a10e51d6ae6e159ff0262d59a
Glebov Boris <avatar29A@gmail.com>
2014-09-11 18:01:51 +0600

    удалил субмодули

---
+++ src/Hqub.Mellody.Core/Utilities/Cache.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hqub.Mellody.Core.Utilities
+{
+    public class Cache
+    {
+        #region .ctor
+
+        private Cache()
+        {
+
+        }
+
+        #endregion
+
+        #region Singleton
+
+        private static Cache _instance;
+        public static Cache Instance
+        {
+            get { return _instance = _instance ?? new Cache(); }
+        }
+
+        #endregion
+    }
+}
