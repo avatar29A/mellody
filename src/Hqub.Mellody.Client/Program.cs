e36a6cbd55430ea3a8ef6546994049dd7fcdeb93
Glebov Boris <avatar29A@gmail.com>
2014-09-15 02:14:58 +0600

    update example for vk tests

--- src/Hqub.Mellody.Client/Program.cs
+++ src/Hqub.Mellody.Client/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Media3D;
+using Hqub.Mellowave.Vkontakte.API.LongPoll;
 
 namespace Hqub.Mellody.Client
 {
@@ -11,10 +12,82 @@ namespace Hqub.Mellody.Client
     {
         public static void Main(string[] args)
         {
-            GetMyAudioRecords();
+//            GetMyAudioRecords();
+//            SearchScorpions();
+//            GetLongPollServer();
+            StartLongPollServer();
+//            SendMessage(6666100, "Лови подборку", "audio9203645_80885922,audio4343194_89404022,audio3830978_72952673,audio-21504294_90590072,audio8236081_34095877,audio808376_123428,audio2519124_91781028,audio1761644_71747984,audio4314080_103754952,audio-21186282_88371893,audio18877023_90175747,audio38682_81635782,audio104349233_107043600,audio43280774_84872021");
+//            GetDialogs();
+//            GetMessages();
+
 
             Console.ReadKey();
         }
+
+        public static void GetDialogs()
+        {
+            var token = GetToken();
+
+            var api = Mellowave.Vkontakte.API.Factories.ApiFactory.Instance(token);
+            var dialogApi = api.GetMessageProduct();
+
+            var response = dialogApi.GetDialogs();
+
+            foreach (var dialog in response.Dialogs)
+            {
+                Console.WriteLine("[{0}]\n\n{1}\n", dialog.Message.UserId, dialog.Message.Body);
+            }
+        }
+
+        public static void GetLongPollServer()
+        {
+            var token = GetToken();
+
+            var api = Mellowave.Vkontakte.API.Factories.ApiFactory.Instance(token);
+            var message = api.GetMessageProduct();
+
+            var response = message.GetLongPollServer();
+
+            Console.WriteLine(response);
+        }
+
+        public static void StartLongPollServer()
+        {
+            var token = GetToken();
+
+            var api = Mellowave.Vkontakte.API.Factories.ApiFactory.Instance(token);
+
+            var longPollServer = LongPollServer.Connect(api);
+            longPollServer.ReceiveData += Console.WriteLine;
+        }
+
+        public static void GetMessages()
+        {
+            var token = GetToken();
+
+            var api = Mellowave.Vkontakte.API.Factories.ApiFactory.Instance(token);
+            var message = api.GetMessageProduct();
+
+            var response = message.Get();
+
+            foreach (var msg in response.Messages)
+            {
+                Console.WriteLine("[{0}]\n\n{1}\n", msg.UserId, msg.Body);
+            }
+        }
+
+        public static void SendMessage(int userId, string text, string attachment="")
+        {
+            var token = GetToken();
+
+            var api = Mellowave.Vkontakte.API.Factories.ApiFactory.Instance(token);
+            var message = api.GetMessageProduct();
+
+            var response = message.Send(userId, message: text, attachment: attachment);
+
+            Console.WriteLine("Message send with ID: {0}", response);
+        }
+
 
         public static void GetMyAudioRecords()
         {
@@ -28,7 +101,9 @@ namespace Hqub.Mellody.Client
             var counter = 0;
             foreach (var track in response.Tracks)
             {
-                Console.WriteLine("{0}. {1} - {2}", ++counter, track.Artist, track.Title);
+                string track_id = string.Format("{0}_{1}", track.OwnerId, track.Id);
+                
+                Console.WriteLine("{0}. {1} - {2} ({3})", ++counter, track.Artist, track.Title, track_id);
             }
         }
 
@@ -44,7 +119,8 @@ namespace Hqub.Mellody.Client
             var counter = 0;
             foreach (var track in response.Tracks)
             {
-                Console.WriteLine("{0}. {1} - {2}", ++counter, track.Artist, track.Title);
+                string track_id = string.Format("{0}_{1}", track.OwnerId, track.Id);
+                Console.WriteLine("{0}. {1} - {2} ({3})", ++counter, track.Artist, track.Title, track_id);
             }
         }
 
