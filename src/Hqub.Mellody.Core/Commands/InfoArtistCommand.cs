edc560bc9d70507ea756dd44d594c308a400612b
Glebov Boris <avatar29A@gmail.com>
2015-03-02 19:49:57 +0500

    Реализовал информационные команды

---
+++ src/Hqub.Mellody.Core/Commands/InfoArtistCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hqub.Mellody.Core.Commands
+{
+    public class InfoArtistCommand : ICommand
+    {
+        public InfoArtistCommand()
+        {
+            Entities = new List<Entity>();
+        }
+
+        public string Name
+        {
+            get { return "InfoArtistCommand"; }
+        }
+
+        public List<Entity> Entities { get; set; }
+    }
+}
